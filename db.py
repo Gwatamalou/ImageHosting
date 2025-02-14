30b373285eea8c1b76766c5b983f40fb64eddd6a
Baton <itrebenkov@gmail.com>
2025-02-14 15:28:36 +0600

    added redis client

--- db.py
+++ db.py
@@ -1,18 +1,22 @@
 from contextlib import asynccontextmanager
-
+import os
+import redis
 from aiobotocore.session import get_session
+from dotenv import load_dotenv
+
+load_dotenv()
 
 
 class S3Client:
     def __init__(self,
-                 access_key: str,
-                 secret_key: str,
-                 endpoint_url: str,
-                 bucket_name: str
+                 access_key: str = os.getenv("s3_access_key"),
+                 secret_key: str = os.getenv("s3_secret_key"),
+                 endpoint_url: str = os.getenv("s3_endpoint_url"),
+                 bucket_name: str = os.getenv("s3_bucket_name")
                  ):
         self.config = {
             "aws_access_key_id": access_key,
-            "aws_secret_access_id": secret_key,
+            "aws_secret_access_key": secret_key,
             "endpoint_url": endpoint_url,
         }
         self.bucket_name = bucket_name
@@ -24,3 +28,15 @@ class S3Client:
             yield client
 
 
+
+
+class RedisClient:
+    def __init__(self,
+                 host: str = "redis",
+                 port: str = "6379",
+                 decode: bool = True):
+        self.client = redis.Redis(host=host, port=port, decode_responses=decode)
+
+
+    def get_client(self):
+        return self.client
